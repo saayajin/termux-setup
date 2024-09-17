f0a9606ec533741c99b2188e55fdac0dc62450e2
saayajin <ssserif@protonmail.com>
2024-09-17 09:26:18 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # termux-setup
+
+## Install
+
+Run main setup script
+
+```bash
+chsh -s bash && bash <(curl https://raw.githubusercontent.com/saayajin/termux-setup/notmain/setup.sh)
+```
+
+**MAKE A BACKUP OF YOUR FILES BEFORE RUNNING THE SCRIPT**
+
+the following folders/files will be lost:
+
+- ~/.config/rclone/rclone.conf
+- ~/.motd/
+- ~/.oh-my-zsh/
+- ~/.shortcuts/icons/
+- ~/.shortcuts/tasks/switch-acodex
+- ~/.shortcuts/tasks/switch-ftp
+- ~/.shortcuts/tasks/switch-scrt
+- ~/.shortcuts/clipboard
+- ~/.shortcuts/del-ssh
+- ~/.shortcuts/new-ssh
+- ~/.shortcuts/rish-shell
+- ~/.termux-setup/
+- ~/.termux/colors.properties
+- ~/.termux/font.ttf
+- ~/.vimrc
+- ~/.zshrc
+- ~/bin/termux-file-editor
+- ~/bin/termux-url-opener
--- setup.sh
+++ setup.sh
@@ -12,29 +12,34 @@ termux-setup-storage
 pkg update -y
 
 # install packages
-pkg in -y aria2 ffmpeg git megatools python tdl termux-api wget zsh
+pkg in -y aria2 exiftool ffmpeg figlet ghostscript git imagemagick megatools openssh python rclone rust sshpass sqlite tdl termux-api termux-services tmux vim wget zsh
 
 # install pip packages
-pip install gallery_dl mutagen yt-dlp spotdl==3.9.6
+pip install csvkit gallery_dl mutagen yt-dlp spotdl==3.9.6
 
 # create dirs
-mkdir -p ~/storage/downloads/Termux/{Aria2,GalleryDL,Git,Mega,Music,Telegram,Videos,Websites,Wget}
+mkdir -p ~/storage/downloads/Termux/{Aria2,downloads,GalleryDL,Git,Mega,Music,Telegram,Videos,Websites,Wget}
 
-#################################
-# TERMUX-URL-OPENER + CLIPBOARD #
-#################################
+######################################################
+# TERMUX-URL-OPENER + TERMUX-FILE-EDITOR + SHORTCUTS #
+######################################################
 
 # remove old repo dir
 rm -rf ~/.termux-setup
 
 # clone repo
-git clone --single-branch --branch main https://github.com/saayajin/termux-setup.git ~/.termux-setup
+git clone --single-branch --branch notmain https://github.com/saayajin/termux-setup.git ~/.termux-setup
 
 # copy home dir
-cp -rp ~/.termux-setup/home /data/data/com.termux/files
+cp -rf ~/.termux-setup/home /data/data/com.termux/files
+
+# symlink
+ln -s ~/storage/downloads/Termux/downloads ~/
 
 # make executable
-chmod u+x ~/.shortcuts/clipboard
+chmod u+x ~/.shortcuts/*
+chmod u+x ~/.shortcuts/tasks/*
+chmod u+x ~/bin/termux-file-editor
 chmod u+x ~/bin/termux-url-opener
 
 #############
@@ -48,7 +53,7 @@ rm -rf ~/.oh-my-zsh
 git clone --single-branch --branch master https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 
 # copy template
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+cp -f ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
 # change theme
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' ~/.zshrc
@@ -58,6 +63,131 @@ echo -e 'prompt_context() {\n  if [[ "$USER" != "$DEFAULT_USER" || -n "$SSH_CLIE
 
 # change shell
 chsh -s zsh
+
+#########
+# ALIAS #
+#########
+
+# set update alias
+echo -e 'alias updateall="pkg update && pkg upgrade -y && pkg clean && pip install --upgrade csvkit gallery-dl mutagen yt-dlp"\n' >> ~/.zshrc
+
+###############
+# REWIND      #
+# BY LARAIB07 #
+###############
+
+# get rewind
+wget --no-check-certificate "https://raw.githubusercontent.com/laraib07/TermuxBackupTools/master/rewind"
+
+# change backup export dir
+sed -i 's+storage/emulated/0/Termux/Backup+storage/emulated/0/backups/Termux+' rewind
+
+# make executable
+chmod u+x rewind
+
+# move to bin
+mv rewind "$PREFIX/bin/"
+
+################
+# TERMUX-MOTD  #
+# BY GENERATOR #
+################
+
+# remove old repo dir
+rm -rf ~/.motd
+
+# clone repo
+git clone --single-branch --branch main https://github.com/saayajin/termux-motd.git ~/.motd
+
+# make executable
+chmod +x ~/.motd/init.sh
+
+# run init.sh on login
+echo "$HOME/.motd/init.sh" > ~/.zprofile
+
+# clear default motd
+echo "" > /data/data/com.termux/files/usr/etc/motd
+
+#######################
+# ALLOW EXTERNAL APPS #
+#######################
+
+# uncomment string
+sed -i 's/# allow-external-apps/allow-external-apps/' ~/.termux/termux.properties
+
+#############
+# BOOKMARKS #
+#############
+
+# remove old dir
+rm -rf ~/bookmarks
+
+# remake dir
+mkdir ~/bookmarks
+
+# add to cdpath
+echo -e 'export CDPATH=~/bookmarks\n' >> ~/.zshrc
+
+# make symlink
+ln -s ~/storage/shared/Documents/git ~/bookmarks
+
+##########
+# OXIPNG #
+##########
+
+# remove old repo dir
+rm -rf ~/.oxipng
+
+# clone repo
+git clone --single-branch --branch master https://github.com/shssoichiro/oxipng.git ~/.oxipng
+
+# change working dir
+cd ~/.oxipng || exit
+
+# build
+cargo build --release
+
+# copy to bin
+cp -f ~/.oxipng/target/release/oxipng $PREFIX/bin
+
+# remove leftovers
+pkg un -y rust
+rm -rf ~/.cargo
+rm -rf ~/.oxipng
+
+###########
+# SCRIPTS #
+###########
+
+# remove old repo dir
+rm -rf ~/.scripts
+
+# clone repo
+git clone --single-branch --branch main https://gist.github.com/a71c14d7c343cd862315e2eeffad5fed.git ~/.scripts
+
+# change working dir
+cd ~/.scripts || exit
+
+# make executable
+chmod u+x *
+
+# make alias for each script
+for file in *.sh; do 
+  OLD="alias ${file%.sh}='. ~/.scripts/$file'"
+  sed -i "s|$OLD|deletemepls|" ~/.zshrc
+  sed -i '/deletemepls/d' ~/.zshrc
+  echo -e "alias ${file%.sh}='. ~/.scripts/$file'" >> ~/.zshrc
+done
+
+##########
+# FINISH #
+##########
+
+# return to home
+cd ~ || exit
+
+# clear pkg cache
+pkg clean
 
 # send toast
 termux-toast -g top -b white -c black Setup done
